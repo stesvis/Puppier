dfbd4fbf3d4e87408616dd1b2d1b50e617d7d691
Cristian Merli <stesvis@gmail.com>
2020-09-11 17:07:48 -0600

    Working on home search form

--- src/components/InputWithIcon.jsx
+++ src/components/InputWithIcon.jsx
@@ -2,15 +2,17 @@ import { Form } from "react-bootstrap";
 import React from "react";
 
 export default function InputWithIcon(props) {
-  const { type, value, placeholder, icon, className } = props;
+  const { type, name, value, placeholder, icon, className, onChange } = props;
 
   return (
     <div className="input-with-icon">
       <Form.Control
         type={type}
+        name={name}
         className={className}
         placeholder={placeholder}
         value={value}
+        onChange={onChange}
       />
       <i className={icon}></i>
     </div>
--- src/components/pages/home/HomeSearchForm.jsx
+++ src/components/pages/home/HomeSearchForm.jsx
@@ -1,58 +1,126 @@
-import { Col, Form, FormControl, FormGroup, Row } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Form,
+  FormControl,
+  FormGroup,
+  Row,
+} from "react-bootstrap";
 
 import InputWithIcon from "../../InputWithIcon";
 import { Link } from "react-router-dom";
 import React from "react";
+import { useState } from "react";
 
 export default function HomeSearchForm() {
+  const [keywords, setKeywords] = useState("");
+  const [location, setLocation] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    let { name, value } = event.target; // extract those two values
+
+    switch (name) {
+      case "keywords":
+        setKeywords(value);
+        break;
+
+      case "location":
+        setLocation(value);
+        break;
+
+      case "category":
+        setCategory(value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     // <!-- ---------- Search Form -->
     <div className="full-search-2 italian-search hero-search-radius">
       <div className="hero-search-content">
-        <Row>
-          <Col lg={4} md={4} sm={12} className="small-padd">
-            <FormGroup>
-              <InputWithIcon
-                type="text"
-                className="b-r"
-                placeholder="Keywords..."
-                icon="ti-search"
-              />
-            </FormGroup>
-          </Col>
+        <Form onSubmit={handleSubmit}>
+          <Row>
+            <Col lg={4} md={4} sm={12} className="small-padd">
+              <FormGroup>
+                <InputWithIcon
+                  type="text"
+                  name="keywords"
+                  className="b-r"
+                  placeholder="Keywords..."
+                  icon="ti-search"
+                  value={keywords}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </Col>
 
-          <Col lg={3} md={3} sm={12} className="small-padd">
-            <FormGroup>
-              <InputWithIcon
-                type="text"
-                className="b-r"
-                placeholder="Location..."
-                icon="ti-target"
-              />
-            </FormGroup>
-          </Col>
+            <Col lg={3} md={3} sm={12} className="small-padd">
+              <FormGroup>
+                <InputWithIcon
+                  type="text"
+                  name="location"
+                  className="b-r"
+                  placeholder="Location..."
+                  icon="ti-target"
+                  value={location}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </Col>
 
-          <Col lg={3} md={3} sm={6} className="small-padd">
-            <FormGroup>
-              <div className="input-with-icon">
-                <FormControl as="select" id="list-category" custom>
-                  <option value="">&nbsp;</option>
-                  <option value="1">Dog</option>
-                  <option value="2">Cat</option>
-                </FormControl>
-                <i className="fa fa-dog"></i>
-              </div>
-            </FormGroup>
-          </Col>
+            <Col lg={3} md={3} sm={6} className="small-padd">
+              <FormGroup>
+                <div className="input-with-icon">
+                  <FormControl
+                    as="select"
+                    id="list-category"
+                    name="category"
+                    custom
+                    value={category}
+                    onChange={handleChange}
+                  >
+                    <option value="">&nbsp;</option>
+                    <option value="1">Dog</option>
+                    <option value="2">Cat</option>
+                  </FormControl>
+                  <i className="fa fa-dog"></i>
+                </div>
+              </FormGroup>
+            </Col>
 
-          <Col lg={2} md={2} sm={12} className="small-padd">
-            <FormGroup>
-              <Link to="#" className="btn search-btn">
-                Search
-              </Link>
-            </FormGroup>
-          </Col>
-        </Row>
+            <Col lg={2} md={2} sm={12} className="small-padd">
+              <FormGroup>
+                {/* <Link
+                  to={`/listings?keywords=${keywords}`}
+                  className="btn search-btn"
+                  onClick={handleSearch}
+                >
+                  Search
+                </Link> */}
+                <Button
+                  type="submit"
+                  className="btn search-btn"
+                  style={{ border: "none" }}
+                >
+                  Search
+                </Button>
+              </FormGroup>
+            </Col>
+          </Row>
+        </Form>
       </div>
     </div>
     // ---------- Search Form
